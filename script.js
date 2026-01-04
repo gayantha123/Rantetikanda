da4118890565f6ea5e06d18136b18480b7f76113
GayanthaLochana0 <lochanagayantha70@gmail.com>
2026-01-04 12:29:42 +0530

    Update default donors with new entries
    Signed-off-by: GayanthaLochana0 <lochanagayantha70@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Default donors
 const defaultDonors = [
+    { photo: "14.png", name: "ගරු සභාපති සුමනදාස ", description: "රිදිගම ප්‍රාදේශිය සභාව මාර්ගය සකස් කිරිමට යන්ත්‍ර සුත්‍ර ලබාදිමට කටයුතු සිදු කිරිම" },
+    { photo: "12.png", name: "විදුලි අධිකාරී ES ප්‍රසාද් මහතා", description: "කඩිනමින් විදුලිය ලබාදිමට කටයුතු සිදුකිරිම" },
+    { photo: "13.png", name: "TO විඡය බන්ඩාර මහතා", description: "මාර්ගය සකස් කිරිමට යන්ත්‍ර සුත්‍ර ලබාදිමට කටයුතු සිදු කිරිම" },
     { photo: "1.jpg", name: "සුරංග කොන්ගහ ගෙදර", description: "" },
     { photo: "2.jpeg", name: "ප්‍රබාත්", description: "" },
     { photo: "3.jpg", name: "ප්‍රසන්න ගීත් කුලියාපිටිය", description: "" }
+   
 ];
 
 let donors = [];
@@ -127,4 +131,3 @@ document.getElementById('bulkForm').addEventListener('submit', function(e) {
 
 // Initialize
 loadDonors();
-
